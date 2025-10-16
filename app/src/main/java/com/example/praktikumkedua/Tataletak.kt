f73e6a40b2914692f5855bb1a1aa4957d826b1e3
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-10-16 16:37:13 +0700

    menambahkan gambar, dan mengganti variable nama gambar

--- app/src/main/java/com/example/praktikumkedua/Tataletak.kt
+++ app/src/main/java/com/example/praktikumkedua/Tataletak.kt
@@ -96,7 +96,7 @@ fun TataletakRowColumn(modifier: Modifier){
 
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier){
-    val gambar = painterResource(id = R.drawable.notasibalok)
+    val gambar = painterResource(id = R.drawable.kumarkumar)
     Column {
         Box(
             modifier = modifier
