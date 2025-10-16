11c4da07b15672d4d4e8647b374a1a21cd4f62d8
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-10-16 16:10:48 +0700

    Menambahkan ColoumnRow dan di dalamnya Row berisi text

--- app/src/main/java/com/example/praktikumkedua/Tataletak.kt
+++ app/src/main/java/com/example/praktikumkedua/Tataletak.kt
@@ -59,3 +59,14 @@ fun TataletakBox(modifier: Modifier) {
     }
 }
 
+@Composable
+fun TataletakColoumnRow(modifier: Modifier){
+    Coloumn(){
+        Row(modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly){
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponen2Baris1")
+            Text(text = "Komponen3Baris1")
+        }
+    }
+}
