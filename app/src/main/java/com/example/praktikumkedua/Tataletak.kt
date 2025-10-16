c6496881950abfd738f441bf75efcb3af70cf9bd
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-10-16 16:32:08 +0700

    memasukan text di bawah Row kedua di dalam Column kedua di bagian TataletakBoxColumnRow

--- app/src/main/java/com/example/praktikumkedua/Tataletak.kt
+++ app/src/main/java/com/example/praktikumkedua/Tataletak.kt
@@ -117,7 +117,11 @@ fun TataletakBoxColumnRow(modifier: Modifier){
                 Row(
                     modifier = modifier.fillMaxWidth(),
                     horizontalArrangement = Arrangement.SpaceEvenly
-                )
+                ){
+                    Text(text = "Col1_Row2_Komponen1")
+                    Text(text = "Col1_Row2_Komponen2")
+                    Text(text = "Col1_Row2_Komponen3")
+                }
             }
         }
     }
