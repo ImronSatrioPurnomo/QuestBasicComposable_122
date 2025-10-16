0f9e697539f732640485c4af58a1dbedefbc74c4
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-10-16 16:41:58 +0700

    menambahkan Image dan text di bawah Spacer

--- app/src/main/java/com/example/praktikumkedua/Tataletak.kt
+++ app/src/main/java/com/example/praktikumkedua/Tataletak.kt
@@ -131,6 +131,17 @@ fun TataletakBoxColumnRow(modifier: Modifier){
                 .height(height = 300.dp)
                 .background(color = Color.Cyan),
             contentAlignment = Alignment.Center
-        )
+        ){
+            Image(painter = gambar,
+                contentDescription = null,
+                contentScale = ContentScale.Fit)
+            Text(text = "Kumar",
+                fontSize = 50.sp,
+                color = Color.Blue,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                modifier = Modifier.align(
+                    alignment = Alignment.Center))
+        }
     }
 }
