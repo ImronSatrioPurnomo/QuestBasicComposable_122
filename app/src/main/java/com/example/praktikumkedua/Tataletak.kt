3e71eeebc3d97b8f92f54fa9a641c893c5e3ac1b
ImronSatrioPurnomo <satriogames2223@gmail.com>
2025-10-16 16:31:11 +0700

    menambahkan Row kedua di dalam Column kedua

--- app/src/main/java/com/example/praktikumkedua/Tataletak.kt
+++ app/src/main/java/com/example/praktikumkedua/Tataletak.kt
@@ -114,6 +114,10 @@ fun TataletakBoxColumnRow(modifier: Modifier){
                     Text(text = "Col1_Row1_Komponen2")
                     Text(text = "Col1_Row1_Komponen3")
                 }
+                Row(
+                    modifier = modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                )
             }
         }
     }
